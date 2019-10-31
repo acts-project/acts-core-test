b29ee90397864b4034df3c0a819e9b0f5383a5ac
Paul Gessinger <hello@paulgessinger.com>
2019-10-31 12:43:18 +0100

    begin implementing new navigator on top of fsm

--- Core/include/Acts/Propagator/Navigator.hpp
+++ Core/include/Acts/Propagator/Navigator.hpp
@@ -13,11 +13,13 @@
 #include <iterator>
 #include <sstream>
 #include <string>
+
 #include "Acts/Geometry/BoundarySurfaceT.hpp"
 #include "Acts/Geometry/Layer.hpp"
 #include "Acts/Geometry/TrackingGeometry.hpp"
 #include "Acts/Geometry/TrackingVolume.hpp"
 #include "Acts/Geometry/detail/BoundaryIntersectionSorter.hpp"
+#include "Acts/Propagator/NavigationOptions.hpp"
 #include "Acts/Propagator/Propagator.hpp"
 #include "Acts/Propagator/detail/ConstrainedStep.hpp"
 #include "Acts/Surfaces/Surface.hpp"
@@ -25,58 +27,6 @@
 namespace Acts {
 
 using Cstep = detail::ConstrainedStep;
-
-/// @brief struct for the Navigation options that are forwarded to
-///        the geometry
-///
-/// @tparam propagator_state_t Type of the object for navigation state
-/// @tparam object_t Type of the object for navigation to check against
-template <typename object_t>
-struct NavigationOptions {
-  /// The navigation direction
-  NavigationDirection navDir = forward;
-
-  /// The boundary check directive
-  BoundaryCheck boundaryCheck = true;
-
-  // How to resolve the geometry
-  /// Always look for sensitive
-  bool resolveSensitive = true;
-  /// Always look for material
-  bool resolveMaterial = true;
-  /// always look for passive
-  bool resolvePassive = false;
-
-  /// object to check against: at start
-  const object_t* startObject = nullptr;
-  /// object to check against: at end
-  const object_t* endObject = nullptr;
-
-  /// Target surface to exclude
-  const Surface* targetSurface = nullptr;
-
-  double pathLimit = std::numeric_limits<double>::max();
-
-  /// Constructor
-  ///
-  /// @param nDir Navigation direction prescription
-  /// @param bcheck Boundary check for the navigation action
-  /// @param sobject Start object to check against
-  /// @param eobject End object to check against
-  /// @param maxStepLength Maximal step length to check against
-  NavigationOptions(NavigationDirection ndir, BoundaryCheck bcheck,
-                    bool resolves = true, bool resolvem = true,
-                    bool resolvep = false, const object_t* sobject = nullptr,
-                    const object_t* eobject = nullptr)
-      : navDir(ndir),
-        boundaryCheck(std::move(bcheck)),
-        resolveSensitive(resolves),
-        resolveMaterial(resolvem),
-        resolvePassive(resolvep),
-        startObject(sobject),
-        endObject(eobject),
-        pathLimit(ndir * std::numeric_limits<double>::max()) {}
-};
 
 /// Navigator class
 ///
