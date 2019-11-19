479cf98c71d342d481d2107483c7370247d18751
Xiaocong Ai <xiaocong.ai@cern.ch>
2019-11-19 12:13:27 +0100

    Add the TrackStateTypeFlag for TrackState

--- Core/include/Acts/EventData/TrackState.hpp
+++ Core/include/Acts/EventData/TrackState.hpp
@@ -17,6 +17,17 @@
 
 namespace Acts {
 
+/// @enum TrackStateFlag
+///
+/// This enum describes the type of TrackState
+enum TrackStateFlag {
+  MeasurementFlag = 0,
+  ParameterFlag = 1,
+  OutlierFlag = 2,
+  HoleFlag = 3,
+  NumTrackStateFlags = 4
+};
+
 class Surface;
 
 /// @class TrackState
@@ -44,6 +55,7 @@ class TrackState {
   /// @param m The measurement object
   TrackState(SourceLink m) : m_surface(&m.referenceSurface()) {
     measurement.uncalibrated = std::move(m);
+    m_typeFlags.set(MeasurementFlag);
   }
 
   /// Constructor from parameters
@@ -53,6 +65,7 @@ class TrackState {
   TrackState(parameters_t p) {
     m_surface = &p.referenceSurface();
     parameter.predicted = std::move(p);
+    m_typeFlags.set(ParameterFlag);
   }
 
   /// Virtual destructor
@@ -64,7 +77,8 @@ class TrackState {
   TrackState(const TrackState& rhs)
       : parameter(rhs.parameter),
         measurement(rhs.measurement),
-        m_surface(rhs.m_surface) {}
+        m_surface(rhs.m_surface),
+        m_typeFlags(rhs.m_typeFlags) {}
 
   /// Copy move constructor
   ///
@@ -72,7 +86,8 @@ class TrackState {
   TrackState(TrackState&& rhs)
       : parameter(std::move(rhs.parameter)),
         measurement(std::move(rhs.measurement)),
-        m_surface(std::move(rhs.m_surface)) {}
+        m_surface(std::move(rhs.m_surface)),
+        m_typeFlags(std::move(rhs.m_typeFlags)) {}
 
   /// Assignment operator
   ///
@@ -81,6 +96,7 @@ class TrackState {
     parameter = rhs.parameter;
     measurement = rhs.measurement;
     m_surface = rhs.m_surface;
+    m_typeFlags = rhs.m_typeFlags;
     return (*this);
   }
 
@@ -91,11 +107,26 @@ class TrackState {
     parameter = std::move(rhs.parameter);
     measurement = std::move(rhs.measurement);
     m_surface = std::move(rhs.m_surface);
+    m_typeFlags = std::move(rhs.m_typeFlags);
     return (*this);
   }
 
   /// @brief return method for the surface
   const Surface& referenceSurface() const { return (*m_surface); }
+
+  /// @brief set the type flag
+  void setType(const TrackStateFlag& flag, bool status = true) {
+    m_typeFlags.set(flag, status);
+  }
+
+  /// @brief test if the tracks state is flagged as a given type
+  bool isType(const TrackStateFlag& flag) const {
+    assert(flag < NumTrackStateFlags);
+    return m_typeFlags.test(flag);
+  }
+
+  /// @brief return method for the type flags
+  std::bitset<NumTrackStateFlags> type() const { return m_typeFlags; }
 
   /// @brief number of Measured parameters, forwarded
   /// @note This only returns a value if there is a calibrated measurement
@@ -141,5 +172,7 @@ class TrackState {
  private:
   /// The surface of this TrackState
   const Surface* m_surface = nullptr;
+  /// The type flag of this TrackState
+  std::bitset<NumTrackStateFlags> m_typeFlags;
 };
 }  // namespace Acts
