88078b42746b6d2184f999acf80c2947a432ddac
Fabian Klimpel <fklimpel@cern.ch>
2020-03-18 16:29:21 +0100

    rebase

--- Core/include/Acts/Fitter/KalmanFitter.hpp
+++ Core/include/Acts/Fitter/KalmanFitter.hpp
@@ -577,7 +577,7 @@ class KalmanFitter {
 
             // Transport & bind the state to the current surface
             auto [boundParams, jacobian, pathLength] =
-                stepper.boundState(state.stepping, *surface, true);
+                stepper.boundState(state.stepping, *surface);
 
             // Fill the track state
             trackStateProxy.predicted() = boundParams.parameters();
@@ -589,7 +589,7 @@ class KalmanFitter {
 
             // Transport & get curvilinear state instead of bound state
             auto [curvilinearParams, jacobian, pathLength] =
-                stepper.curvilinearState(state.stepping, true);
+                stepper.curvilinearState(state.stepping);
 
             // Fill the track state
             trackStateProxy.predicted() = curvilinearParams.parameters();
@@ -648,7 +648,7 @@ class KalmanFitter {
 
         // Transport & bind the state to the current surface
         auto [boundParams, jacobian, pathLength] =
-            stepper.boundState(state.stepping, *surface, true);
+            stepper.boundState(state.stepping, *surface);
 
         // Create a detached track state proxy
         auto tempTrackTip =
